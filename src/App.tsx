5d1441ed727e2fc214246d48309c23ac87c1f39f
ShalahuddinAbdul <89372431+1CL0UD@users.noreply.github.com>
2023-12-13 02:24:51 +0700

    1 - Understanding the Effect Hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import { useEffect, useRef } from 'react';
 import './App.css';
 
 function App() {
-  return <></>;
+  const ref = useRef<HTMLInputElement>(null);
+  useEffect(() => {
+    if (ref.current) ref.current.focus();
+  });
+  useEffect(() => {
+    document.title = 'MyApp';
+  });
+
+  return (
+    <div>
+      <input ref={ref} type="text" className="form-control" />
+    </div>
+  );
 }
 
 export default App;
