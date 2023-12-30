dd0a2d47705b321c3a82eeaf3fe65e119b960370
ShalahuddinAbdul <89372431+1CL0UD@users.noreply.github.com>
2023-12-30 07:14:27 +0700

    16 - Create a Generic HTTP Service

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
 
   useEffect(() => {
     setLoading(true);
-    const { request, cancel } = userService.getAllUsers();
+    const { request, cancel } = userService.getAll<User>();
     request
       .then((res) => {
         setUsers(res.data);
@@ -30,7 +30,7 @@ function App() {
   const deleteUser = (user: User) => {
     const originalUsers = [...users];
     setUsers(users.filter((u) => u.id != user.id));
-    userService.deleteUsers(user.id).catch((err: Error) => {
+    userService.delete(user.id).catch((err: Error) => {
       setError(err.message);
       setUsers(originalUsers);
     });
@@ -42,7 +42,7 @@ function App() {
 
     setUsers([newUser, ...users]);
     userService
-      .addUser(newUser)
+      .add(newUser)
       .then(({ data: savedUser }) => setUsers([savedUser, ...users]))
       .catch((err) => {
         setError(err.message);
@@ -54,7 +54,7 @@ function App() {
     const originalUsers = [...users];
     const updatedUser = { ...user, name: user.name + '!' };
     setUsers(users.map((u) => (u.id === user.id ? updatedUser : u)));
-    userService.updateUser(updatedUser).catch((err) => {
+    userService.update(updatedUser).catch((err) => {
       setError(err.message);
       setUsers(originalUsers);
     });
